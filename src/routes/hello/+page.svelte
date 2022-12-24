fe837b9b65b6c38bf1f122bd9facbb8d5eb3571b
Rafa Brown <shane.rafael@zohomail.com>
2022-12-24 02:25:42 -0700

    add Docker files & test page

---
+++ src/routes/hello/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { env } from '$env/dynamic/public';
+</script>
+
+<h1>Hello World!</h1>
+<p>
+	{env.PUBLIC_GREETING}
+	The current node environment is: {process.env.NODE_ENV}
+</p>
+
+<p>This is rebuilt a fifth time.</p>
